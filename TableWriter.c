388d8f9a4f2198a259c81454e6bae298a11d2f02
Prafullsingh975 <96352051+Prafullsingh975@users.noreply.github.com>
2022-02-15 20:48:44 +0530

    Add files via upload

---
+++ TableWriter.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int a, b, c, d, e, f, g, h, i, j, k;
+   printf("Enter number to write table of ");
+   scanf("%d",&a);
+   b= a *1;
+   c= a *2;
+   d= a *3;
+   e= a *4;
+   f= a *5;
+   g= a *6;
+   h= a *7;
+   i= a *8;
+   j= a *9;
+   k= a *10;
+   printf("Tabel of %d is \n",a);
+   printf("%d * 1 = %d \n" ,a ,b);
+   printf("%d * 2 = %d \n" ,a ,c);
+   printf("%d * 3 = %d \n" ,a ,d);
+   printf("%d * 4 = %d \n" ,a ,e);
+   printf("%d * 5 = %d \n" ,a ,f);
+   printf("%d * 6 = %d \n" ,a ,g);
+   printf("%d * 7 = %d \n" ,a ,h);
+   printf("%d * 8 = %d \n" ,a ,i);
+   printf("%d * 9 = %d \n" ,a ,j);
+   printf("%d * 10 = %d \n" ,a ,k);
+   printf("__________THANK YOU__________");
+
+    return 0;
+}
